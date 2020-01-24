3b4c8b847099e528f366467ba5c37b6be8a2d44b
Grace Aredel <kataramoonlight@gmail.com>
2020-01-24 12:57:41 +0300

    base methods for login and register

--- tests_grace/page_objects/login_register_page.py
+++ tests_grace/page_objects/login_register_page.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException, NoSuchElementException, ElementNotInteractableException
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
 
 
 class LoginRegisterPage:
@@ -23,3 +23,37 @@ class LoginRegisterPage:
             return element
         except (NoSuchElementException, TimeoutException):
             return False
+
+
+    def fill_username_email_field(self, name_email):
+
+
+    def fill_log_password_field(self):
+
+
+    def click_login_button(self):
+
+
+    def login(self):
+        self.fill_username_email_field()
+        self.fill_log_password_field()
+        self.click_login_button()
+
+
+    def fill_username_field(self, name):
+
+
+    def fill_email_field(self, email):
+
+
+    def fill_reg_password_field(self, password):
+
+
+    def click_signin_button(self):
+
+
+    def register(self):
+        self.fill_username_field(name)
+        self.fill_email_field(email)
+        self.fill_reg_password_field(password)
+        self.click_signin_button()
