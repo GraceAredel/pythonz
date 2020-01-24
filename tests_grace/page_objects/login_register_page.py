cd17480b96fcd87bd9ae9b746862315c9b34d519
Grace Aredel <kataramoonlight@gmail.com>
2020-01-24 14:11:04 +0300

    base methods

--- tests_grace/page_objects/login_register_page.py
+++ tests_grace/page_objects/login_register_page.py
@@ -1,7 +1,8 @@
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
+
+from tests_grace.locators.LoginPageLocators import *
 
 
 class LoginRegisterPage:
@@ -26,33 +27,42 @@ class LoginRegisterPage:
 
 
     def fill_username_email_field(self, name_email):
+        els = self.driver.find_elements(*LoginPageFieldLocators.USERNAME_OR_EMAIL)
+        field = els[0]
+        field.send_keys(name_email)
 
-
-    def fill_log_password_field(self):
-
+    def fill_log_password_field(self, password):
+        els = self.driver.find_elements(*LoginPageFieldLocators.PASSWORD)
+        field = els[0]
+        field.send_keys(password)
 
     def click_login_button(self):
+        self.driver.find_element(*LoginPageFieldLocators.LOGIN_BUTTON).click()
 
-
-    def login(self):
-        self.fill_username_email_field()
-        self.fill_log_password_field()
+    def login(self, name_email, password):
+        self.fill_username_email_field(name_email)
+        self.fill_log_password_field(password)
         self.click_login_button()
 
-
     def fill_username_field(self, name):
-
+        els = self.driver.find_elements(*RegisterFieldLocators.USERNAME)
+        field = els[1]
+        field.send_keys(name)
 
     def fill_email_field(self, email):
-
+        els = self.driver.find_elements(*RegisterFieldLocators.EMAIL)
+        field = els[1]
+        field.send_keys(email)
 
     def fill_reg_password_field(self, password):
-
+        els = self.driver.find_elements(*RegisterFieldLocators.PASSWORD)
+        field = els[1]
+        field.send_keys(password)
 
     def click_signin_button(self):
+        self.driver.find_element(*RegisterFieldLocators.REGISTER_BUTTON).click()
 
-
-    def register(self):
+    def register(self, name, email, password):
         self.fill_username_field(name)
         self.fill_email_field(email)
         self.fill_reg_password_field(password)
